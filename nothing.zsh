834bcd117f8eb54383379f78b87bc2989e1c1e87
John <john@ahost.adomain>
2024-01-21 12:22:07 -0500

    Customized prompt further

--- nothing.zsh
+++ nothing.zsh
@@ -1,21 +1,20 @@
 #!/usr/bin/env zsh
 
-NT_PROMPT_SYMBOL=$
+NT_PROMPT_SYMBOL=':'
 
 function precmd(){
   autoload -U add-zsh-hook
   setopt prompt_subst
 
-  if [[ "$NT_HIDE_COUNT" == '1' ]]; then
-  	command_count=''
-  else
-  	command_count='%F{gray}%i %f'
-  fi
+  current_time='%F{cyan}[%*]%f'
+  current_active_directory='%F{yellow}[%~]%f'
+  current_user='%F{gray}%n@%m%f'
 
-  PROMPT='${command_count}%(?.%F{green}${NT_PROMPT_SYMBOL}%f.%F{red}${NT_PROMPT_SYMBOL}%f) '
+  PROMPT='${current_time} ${current_active_directory}
+${current_user}%(?.%F{green}${NT_PROMPT_SYMBOL}%f.%F{red}${NT_PROMPT_SYMBOL}%f) '
 
   if [[ "$NT_HIDE_EXIT_CODE" == '1' ]]; then
-  	RPROMPT='%1d'
+  	RPROMPT=''
   else
   	RPROMPT='%(?..%F{red}%B%S  $?  %s%b%f)'
   fi
